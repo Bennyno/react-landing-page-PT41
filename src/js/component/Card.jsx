139d2a8360ae8dd2a605010b0bf35c7ca451709d
Bennyno <ben.fernandez28@gmail.com>
2022-10-02 15:09:51 +0000

    updated pics to cards

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -21,7 +21,7 @@ export function Card() {
           </div>
         </div>
 		<div className="col-sm-3">
-          <img className="card-img-top" src="https://www.pixelstalk.net/wp-content/uploads/2016/10/Blank-Desktop-Wallpaper.jpg" alt="Card image cap" />
+          <img className="card-img-top" src="https://www.pixelstalk.net/wp-content/uploads/2016/10/Blank-Wallpaper-HD.jpg" alt="Card image cap" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
@@ -52,7 +52,7 @@ export function Card() {
           </div>
         </div>
         <div className="col-sm-3">
-          <img className="card-img-top" src="https://www.pixelstalk.net/wp-content/uploads/2016/10/Blank-Desktop-Wallpaper.jpg" alt="Card image cap" />
+          <img className="card-img-top" src="https://www.pixelstalk.net/wp-content/uploads/2016/10/Blank-Wallpaper-HD.jpg" alt="Card image cap" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
